7803527c0e3ed5d3a3c83408372fd9d93b47848f
kalinplus <13658226+kalinplus@user.noreply.gitee.com>
2024-11-01 16:53:37 +0800

    fix a tiny bug in script

--- googleColabRedirectBypass/googleColabRedirectBypass.js
+++ googleColabRedirectBypass/googleColabRedirectBypass.js
@@ -8,37 +8,41 @@
 // @grant        none
 // @license      MIT
 // ==/UserScript==
- 
+
 // link: https://greasyfork.org/en/scripts/515055-google-colab-redirect-bypass
-(function() {
-    'use strict';
- 
-    // Check if the current page is the "Redirect Notice" page
-    const isRedirectPage = () => {
-        return document.title === 'Redirect Notice' && 
-               document.body.innerHTML.includes('The previous page is sending you to');
-    };
- 
-    // Jump to the target link automatically
-    const redirectToTargetLink = () => {
-        const linkElement = document.querySelector('a[href^="https://"]');
-        if (linkElement) {
-            window.location.href = linkElement.href; // 自动跳转
-        }
-    };
- 
-    // Detect the change of the page
-    const observer = new MutationObserver(() => {
-        if (isRedirectPage()) {
-            redirectToTargetLink();
-        }
-    });
- 
-    // Initialize the observation
-    window.addEventListener('load', () => {
-        if (isRedirectPage()) {
-            redirectToTargetLink();
-        }
-        observer.observe(document.body, { childList: true, subtree: true });
-    });
+(function () {
+  "use strict";
+
+  // Check if the current page is the "Redirect Notice" page
+  const isRedirectPage = () => {
+    return (
+      document.title === "Redirect Notice" &&
+      document.body.innerHTML.includes("The previous page is sending you to")
+    );
+  };
+
+  // Jump to the target link automatically
+  const redirectToTargetLink = () => {
+    const linkElement = document.querySelectorAll(
+      'a[href^="https://"], a[href^="http://"]'
+    );
+    if (linkElement.length > 0) {
+      window.location.href = linkElement[0].href; // Jump automatically
+    }
+  };
+
+  // Detect the change of the page
+  const observer = new MutationObserver(() => {
+    if (isRedirectPage()) {
+      redirectToTargetLink();
+    }
+  });
+
+  // Initialize the observation
+  window.addEventListener("load", () => {
+    if (isRedirectPage()) {
+      redirectToTargetLink();
+    }
+    observer.observe(document.body, { childList: true, subtree: true });
+  });
 })();
